aa9cbb34f04ad3927e2fd954896e085634e296c3
Wan Wan Learns <81837764+WanWanLearns@users.noreply.github.com>
2021-05-05 09:44:45 +0800

    Update my.js

--- my.js
+++ my.js
@@ -1,23 +1,20 @@
 function addGS() {
 
-	const url = "https://script.google.com/macros/s/AKfycbz8PTsZATejBB4z1jBBYYzcrAWu1gRMsnMF83i3ytJNrDWrt6p7rrqD8Ig9wfOLfzXThQ/exec";
-
+	const url = "https://script.google.com/macros/s/AKfycbzQ0vhMUimIv3V0Co-MtYYhWEja8cQoBQjCyB6F2otyWeOeBrKQ/exec";
 	fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    //credentials: 'omit', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    //body: JSON.stringify(data) // body data type must match "Content-Type" header
-	body: JSON.stringify({name:"Alvin"}) // body data type must match "Content-Type" header
-  });
-
-
+		method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    		mode: 'no-cors', // no-cors, *cors, same-origin
+    		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    		//credentials: 'omit', // include, *same-origin, omit
+    		headers: {
+      			'Content-Type': 'application/json'
+      			// 'Content-Type': 'application/x-www-form-urlencoded',
+    		},
+    		redirect: 'follow', // manual, *follow, error
+    		//referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    		//body: JSON.stringify(data) // body data type must match "Content-Type" header
+		body: JSON.stringify({name:"Alvin"}) // body data type must match "Content-Type" header
+  	});
 
 }
 
